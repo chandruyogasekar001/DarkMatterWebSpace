e659841d0af08c5a8bc01c43f2edbd21f2d6fc05
chandruyogasekar01 <chandruyogasekar111@gmail.com>
2024-06-02 12:20:24 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import AppDetails from './AppDetails'; // Imported AppDetails component
 import './App.css';
 import icon1 from './icons8-alien-48.png';
 import icon2 from './icons8-google-blog-search-48.png';
-import icon3 from './icons8-music-60.png';
+import icon3 from './icons8-airpods-pro-max-48.png';
 import icon4 from './icons8-resume-48.png';
 import icon5 from './icons8-human-48.png';
 
 const apps = [
-  { id: 1, icon: icon1, label: 'Galaxy blog', description: 'Description for App 1', website: 'https://blog-app-mern-dun.vercel.app/' },
-  { id: 2, icon: icon2, label: 'Autobot AI', description: 'Description for App 2', website: 'https://autobot-ai-two.vercel.app/' },
+  { id: 1, icon: icon2, label: 'Galaxy blog', description: 'Description for App 1', website: 'https://blog-app-mern-dun.vercel.app/' },
+  { id: 2, icon: icon1, label: 'Autobot AI', description: 'Description for App 2', website: 'https://autobot-ai-two.vercel.app/' },
   { id: 3, icon: icon3, label: 'ParallelWorld', description: 'Description for App 1', website: 'https://parallel-world-music-mini.vercel.app/' },
   { id: 4, icon: icon4, label: 'Resume Ready', description: 'Description for App 2', website: 'https://chandruyogasekar001.github.io/resumebuilder/' },
   { id: 5, icon: icon5, label: 'My portfolio', description: 'Description for App 2', website: 'https://chandru-portfolio-react-app.vercel.app/' },
